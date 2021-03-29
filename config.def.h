eede8d6d2ce9649e5e62db885e9330064df0f9a7
Glask1 <28818063+Glask1@users.noreply.github.com>
2021-03-29 13:13:10 -0700

    font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"emoji:pixelsize=12:antialias=true:autohint=true", 
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 2;
 
 /*
